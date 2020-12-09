37b602afcf81bd7732dea1924bf11a4b520c77b0
badanga ishak <badangabello@gmail.com>
2020-12-09 10:59:03 -0800

    Updates

---
+++ precache-manifest.c32d4b0065cec18c88be3d661e86f6d4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2cc7eb255895a64f29ae2502bd343a",
+    "url": "/landingpage2/index.html"
+  },
+  {
+    "revision": "ba650690d0a09263edcd",
+    "url": "/landingpage2/static/css/2.58c4053f.chunk.css"
+  },
+  {
+    "revision": "62cdb08fbcc79e808cda",
+    "url": "/landingpage2/static/css/main.0f8b0ecc.chunk.css"
+  },
+  {
+    "revision": "ba650690d0a09263edcd",
+    "url": "/landingpage2/static/js/2.b9434947.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landingpage2/static/js/2.b9434947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62cdb08fbcc79e808cda",
+    "url": "/landingpage2/static/js/main.27bdbd47.chunk.js"
+  },
+  {
+    "revision": "a8bac1c2b71527c58516",
+    "url": "/landingpage2/static/js/runtime-main.be0a03d0.js"
+  },
+  {
+    "revision": "df6601b0961197fd570b44815ac44c68",
+    "url": "/landingpage2/static/media/bg-header-desktop.df6601b0.png"
+  },
+  {
+    "revision": "784789634d6cfa0f540b90d2fe91a0b0",
+    "url": "/landingpage2/static/media/icon-blacklist.78478963.svg"
+  },
+  {
+    "revision": "99ef83bcd798a76238f44aee35ed3f7f",
+    "url": "/landingpage2/static/media/icon-facebook.99ef83bc.svg"
+  },
+  {
+    "revision": "0f2e9e2537a99944fd45700c0931028c",
+    "url": "/landingpage2/static/media/icon-instagram.0f2e9e25.svg"
+  },
+  {
+    "revision": "ee6850bcfba7ae359a07e72ff5a5ad7e",
+    "url": "/landingpage2/static/media/icon-preview.ee6850bc.svg"
+  },
+  {
+    "revision": "d9185a429e69ffbac9f83bd9b7586c68",
+    "url": "/landingpage2/static/media/icon-text.d9185a42.svg"
+  },
+  {
+    "revision": "799c9975d592063e2995e272d7344607",
+    "url": "/landingpage2/static/media/icon-twitter.799c9975.svg"
+  },
+  {
+    "revision": "a030348e47bd6610cc92ab378a8d6059",
+    "url": "/landingpage2/static/media/image-computer.a030348e.png"
+  },
+  {
+    "revision": "6208ead7a0a569d9746e820ee58f5771",
+    "url": "/landingpage2/static/media/image-devices.6208ead7.png"
+  },
+  {
+    "revision": "28caa0248db688b9dfaa91155414ee80",
+    "url": "/landingpage2/static/media/logo.28caa024.svg"
+  }
+]);
